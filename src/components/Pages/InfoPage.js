521ec54cb6f9e4786c92390f28b1fb7fded7af0f
sleepyyuu <96076501+sleepyyuu@users.noreply.github.com>
2022-04-02 19:58:00 -0700

    typo fix

--- src/components/Pages/InfoPage.js
+++ src/components/Pages/InfoPage.js
@@ -10,7 +10,7 @@ export default function InfoPage(props) {
           created in hopes of presenting information regarding renewals in a more accessible manner. The official USCIS
           case processing times, while helpful, is incredibly broad. Many times, I found myself needing to look up and
           compare case times to that of other DACA recipients to make sure my case was still within the acceptable
-          limits of processing time or to make a decision on when to send my renewal.\nThis site parses submissions from
+          limits of processing time or to make a decision on when to send my renewal. This site parses submissions from
           Reddit and processes posts that have usable information regarding application timelines.
         </div>
         <div className="contactHeader">Contact information</div>
